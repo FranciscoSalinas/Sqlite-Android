12a6971f0df0da1fb570bc85e6a57cc536b600a6
FranciscoSalinas <nfsalinasm@unl.edu.ec>
2016-08-05 23:31:30 -0500

    Sqlite-Android
    Conexión a base de datos sqlite en Android

---
+++ FormularioSqlite/app/src/main/java/com/example/inspirion/formulariosqlite/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.inspirion.formulariosqlite;
+
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button all,n;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        all = (Button)findViewById(R.id.elements);
+        n = (Button)findViewById(R.id.new_element);
+
+        all.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, ElementsActivity.class);
+                startActivity(i);
+            }
+        });
+        n.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, NewActivity.class);
+                startActivity(i);
+            }
+        });
+
+    }
+}
